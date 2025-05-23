1721fa2de34bede801be63a8337a918202594404
Marina Kuzmenko <marina.kuzmenko.01@gmail.com>
2025-05-23 11:02:07 +0200

    ДЗ 4: Перевірка теорії ймовірності

---
+++ js_homework4/checkProbability.js
@@ -0,0 +1,27 @@
+function checkProbabilityTheory(count) {
+  let evenCount = 0;     // лічильник парних
+  let oddCount = 0;      // лічильник непарних
+
+  for (let i = 0; i < count; i++) {
+    let number = Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
+
+    if (number % 2 === 0) {
+      evenCount++;
+    } else {
+      oddCount++;
+    }
+  }
+
+  let evenPercentage = (evenCount / count) * 100;
+  let oddPercentage = (oddCount / count) * 100;
+
+  console.log("Кількість згенерованих чисел: " + count);
+  console.log("Парних чисел: " + evenCount);
+  console.log("Непарних чисел: " + oddCount);
+  console.log("Відсоток парних до непарних:");
+  console.log("  Парних: " + evenPercentage.toFixed(2) + "%");
+  console.log("  Непарних: " + oddPercentage.toFixed(2) + "%");
+}
+
+
+checkProbabilityTheory(100);
