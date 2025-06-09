5c40b03ee3223a96f610438bb3fa83f69130900c
Marina Kuzmenko <marina.kuzmenko.01@gmail.com>
2025-06-09 20:28:35 +0200

    Homework 7: words without A or a and more than 6 letters

---
+++ js_homework7/noAwords.js
@@ -0,0 +1,41 @@
+var text = "Wonderful Joyful Happiness Time Task Apple Amazing Flying Magic Strength Victory Freedom";
+
+// Розбиваємо рядок на слова
+var words = text.split(" ");
+
+var suitable = [];
+var unsuitable = [];
+
+// Розділяємо слова
+for (var i = 0; i < words.length; i++) {
+    var word = words[i];
+
+    if (word.length >= 6 && !/[Aa]/.test(word)) {
+        suitable.push(word);
+    } else {
+        unsuitable.push(word);
+    }
+}
+
+// Виводимо результат для підходящих слів
+console.log("✅ Підходять:");
+var suitableLine = "";
+for (var i = 0; i < suitable.length; i++) {
+    suitableLine += suitable[i];
+    if (i < suitable.length - 1) {
+        suitableLine += ", ";
+    }
+}
+console.log(suitableLine);
+
+// Виводимо результат для підходящих слів
+console.log("\n❌ Не підходять:");
+var unsuitableLine = "";
+for (var i = 0; i < unsuitable.length; i++) {
+    unsuitableLine += unsuitable[i];
+    if (i < unsuitable.length - 1) {
+        unsuitableLine += ", ";
+    }
+}
+console.log(unsuitableLine);
+
